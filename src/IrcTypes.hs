9e96423afd6a52eb3a1af9e212bafc058f836cd9
Lúcás Meier <cronokirby@gmail.com>
2018-01-16 19:53:42 +0100

    adds parsing for a subset of irc messages, along with property tests

--- src/IrcTypes.hs
+++ src/IrcTypes.hs
@@ -1,11 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
-module IrcTypes where
+module IrcTypes
+    ( IrcMessage(..)
+    , Channel
+    , Key
+    , Source
+    , joinNonEmpty
+    , joinIrcChannels
+    , sendText
+    , readText
+    , readNonEmpty
+    ) where
 
-import           Data.List.NonEmpty (NonEmpty (..))
-import qualified Data.List.NonEmpty as N
-import           Data.Monoid        ((<>))
-import           Data.Text          (Text)
-import qualified Data.Text          as T
+import           Control.Applicative  ((<|>))
+import           Data.Attoparsec.Text as A (Parser, char, endOfInput, many',
+                                            maybeResult, parseOnly, string,
+                                            take, takeText, takeWhile1)
+import           Data.List.NonEmpty   (NonEmpty (..))
+import qualified Data.List.NonEmpty   as N
+import           Data.Monoid          ((<>))
+import           Data.Text            (Text)
+import qualified Data.Text            as T
 
 -- type synonyms for text
 type Channel = Text
@@ -16,9 +30,9 @@ type Source = Text
 data IrcMessage
     = Ping Text -- ^ A ping from the server, with text to be repeated
     | Pong Text -- ^ A pong sent from the client
-    | Join (NonEmpty Channel) -- ^ Joining a list of channels
-    | Part (NonEmpty Channel) -- ^ Leave a list of channels
-    deriving (Show)
+    | Join (NonEmpty Channel) -- ^ Joining a list of channels, of which no strings may be empty
+    | Part (NonEmpty Channel) -- ^ Leave a list of channels, of which no strings may be empty
+    deriving (Eq, Show)
 
 -- | Joins the elements of a nonempty list by sep
 joinNonEmpty :: Text -> NonEmpty Text -> Text
@@ -29,11 +43,31 @@ joinIrcChannels :: NonEmpty Channel -> Text
 joinIrcChannels = joinNonEmpty "," . fmap (T.cons '#')
 
 -- | Formats an irc message ready for sending
+-- `readText . sendText == Right`
 sendText :: IrcMessage -> Text
 sendText (Ping m)     = "PING :" <> m
 sendText (Pong m)     = "PONG :" <> m
 sendText (Join chans) = "JOIN :" <> joinIrcChannels chans
+sendText (Part chans) = "PART :" <> joinIrcChannels chans
 
+-- | Reads an irc message, without the source
+readText :: Text -> Either String IrcMessage
+readText = parseOnly (foldr1 (<|>) [readPing, readPong, readJoin, readPart])
+  where
+    wrapWith txt pars = string txt *> pars <* endOfInput
+    readPing = Ping <$> wrapWith "PING :" takeText
+    readPong = Pong <$> wrapWith "PONG :" takeText
+    readJoin = Join <$> wrapWith "JOIN :" (readNonEmpty ',' '#')
+    readPart = Part <$> wrapWith "PART :" (readNonEmpty ',' '#')
+
+-- doesn't parse empty text between seperators
+readNonEmpty :: Char -> Char -> Parser (NonEmpty Text)
+readNonEmpty sep header =
+    (:|)
+    <$> parseItem
+    <*> many' (A.take 1 *> parseItem) -- needed to get rid of the sep
+  where
+    parseItem = char header *> takeWhile1 (/= sep)
 
 
 type ServerMessage = (IrcMessage, Source)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.Text          as T
 import           IrcTypes
 import           Test.Hspec
+import           Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
@@ -11,6 +13,7 @@ main = hspec $ do
 ircTypesSpec = do
     joinNonEmptySpec
     joinIrcChannelsSpec
+    ircMessageSpec
 
 
 joinNonEmptySpec = describe "joinNonEmpty" $ do
@@ -29,3 +32,21 @@ joinIrcChannelsSpec = describe "joinIrcChannels" $ do
         joinIrcChannels ("foo" :| []) `shouldBe` "#foo"
     it "handles multiple channels" $ do
         joinIrcChannels ("foo" :| ["bar", "baz"]) `shouldBe` "#foo,#bar,#baz"
+
+
+instance Arbitrary IrcMessage where
+    arbitrary = do
+        t1 <- textGen
+        t2 <- textGen
+        (joinT : joinTs) <- listOf1 channelGen
+        (partT : partTs) <- listOf1 channelGen
+        elements [Ping t1, Ping t2, Join (joinT :| joinTs), Part (partT :| partTs)]
+      where
+        textGen = T.pack . getPrintableString <$> arbitrary
+        -- channels can't be empty or contain , or #
+        channelGen = T.cons 'a' . T.filter (not . (`elem` [',', '#'])) <$> textGen
+
+
+ircMessageSpec = describe "readText" $ do
+    it "is inverse to sendText" $ property $ \x ->
+        (readText . sendText) x == Right (x :: IrcMessage)
